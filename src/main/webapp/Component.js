bfef9a491c1ecf7b45c3588ef925eaddffdc9d03
Jan Penninkhof <jan@penninkhof.com>
2016-06-12 19:50:54 +0000

    Some refactoring based on Roberts version

--- src/main/webapp/Component.js
+++ src/main/webapp/Component.js
@@ -2,8 +2,9 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"com/sap/mentors/lemonaid/model/models",
-	"openui5/googlemaps/ScriptsUtil"
-], function(UIComponent, Device, models, gmapScriptsUtil) {
+	"com/sap/mentors/lemonaid/controller/ErrorHandler",
+	"com/sap/mentors/lemonaid/util/openui5_googlemaps/ScriptsUtil"
+], function(UIComponent, Device, models, ErrorHandler, gmapScriptsUtil) {
 	"use strict";
 
 	return UIComponent.extend("com.sap.mentors.lemonaid.Component", {
@@ -21,6 +22,13 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
+			// Register Google Maps
+			var sPath = jQuery.sap.getResourcePath("openui5/googlemaps/loadScripts");
+			jQuery.sap.registerResourcePath("google.maps", sPath);
+
+			// initialize the error handler with the component
+			this._oErrorHandler = new ErrorHandler(this);
+
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 
@@ -30,14 +38,44 @@ sap.ui.define([
 			//TODO - get real key this is W3Cs
        		gmapScriptsUtil.setApiKey('AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM');
 
-
 			// create the views based on the url/hash
 			this.getRouter().initialize();
 
+		},
 
+		/**
+		 * The component is destroyed by UI5 automatically.
+		 * In this method, the ErrorHandler is destroyed.
+		 * @public
+		 * @override
+		 */
+		destroy : function () {
+			this._oErrorHandler.destroy();
+			// call the base component's destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
 
+		/**
+		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+		 * design mode class should be set, which influences the size appearance of some controls.
+		 * @public
+		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+		 */
+		getContentDensityClass : function() {
+			if (this._sContentDensityClass === undefined) {
+				// check whether FLP has already set the content density class; do nothing in this case
+				if (jQuery(document.body).hasClass("sapUiSizeCozy") || jQuery(document.body).hasClass("sapUiSizeCompact")) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
 		}
+
 	});
 
 });
-
