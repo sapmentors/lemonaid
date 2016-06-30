390eb446a0115bc93835642888d313ef75dfdefe
Jan Penninkhof <jan@penninkhof.com>
2016-06-30 23:33:39 +0200

    Add Java logic allowing users to login through /login.html and add json service similar to HCP's userApi allowing to pull user-data once a user has logged in.

---
+++ src/main/java/com/sap/mentors/lemonaid/rest/User.java
@@ -0,0 +1,47 @@
+package com.sap.mentors.lemonaid.rest;
+
+public class User {
+
+	private final String name;
+	private final String firstName;
+	private final String lastName;
+	private final String email;
+	private final String displayName;
+
+	public User(String name) {
+		this.name = name;
+		this.firstName = null;
+		this.lastName = null;
+		this.email = null;
+		this.displayName = null;
+	}
+
+	public User(String name, String firstName, String lastName, String email, String displayName) {
+		this.name = name;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.displayName = displayName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+	
+}
---
+++ src/main/java/com/sap/mentors/lemonaid/rest/UserController.java
@@ -0,0 +1,49 @@
+package com.sap.mentors.lemonaid.rest;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sap.security.um.service.UserManagementAccessor;
+import com.sap.security.um.user.UserProvider;
+
+@RestController
+@RequestMapping("/userapi/currentUser")
+public class UserController {
+
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping(method = RequestMethod.GET)
+    User getUser(HttpServletRequest request) {
+    	Principal userPrincipal = request.getUserPrincipal();
+		if (userPrincipal != null) {
+            try {
+			    // UserProvider provides access to the user storage
+			    UserProvider users = UserManagementAccessor.getUserProvider();
+	
+			    // Read the currently logged in user from the user storage
+			    com.sap.security.um.user.User user = users.getUser(userPrincipal.getName());
+	
+			    // Print the user name and email
+			    return new User(
+			    		userPrincipal.getName(),
+			    		user.getAttribute("firstname"),
+			    		user.getAttribute("lastname"),
+			    		user.getAttribute("email"),
+			    		user.getAttribute("firstname") + " " + user.getAttribute("lastname") + " (" + userPrincipal.getName() + ")"
+			    	);
+			} catch (Exception e) {
+				log.error("Error: " + e.getMessage());
+			}			
+	        return new User(userPrincipal.getName());
+		}
+        return null;
+    }
+
+}
