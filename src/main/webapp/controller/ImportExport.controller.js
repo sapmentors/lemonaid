5fd4576cf060fa8b7e21402b6216a97962108cf0
Jan Penninkhof <jan@penninkhof.com>
2016-07-14 13:13:17 +0200

    Add placeholder for import/export page

---
+++ src/main/webapp/controller/ImportExport.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    "com/sap/mentors/lemonaid/controller/BaseController"
+], function(BaseController) {
+    "use strict";
+
+    return BaseController.extend("com.sap.mentors.lemonaid.controller.ImportExport", {
+
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
+
+		/**
+		 * Called when the view controller is instantiated.
+		 * @public
+		 */
+        onInit: function() {
+        }
+
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
+
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+    });
+
+});
