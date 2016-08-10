b5f76e02ffd9e5185f1392d8d734b3cdbfcefa69
Jan Penninkhof <jan@penninkhof.com>
2016-08-10 11:39:06 +0000

    Textual changes

--- src/main/webapp/controller/ImportExport.controller.js
+++ src/main/webapp/controller/ImportExport.controller.js
@@ -155,6 +155,10 @@ sap.ui.define([
 			}
 			this.busyDialog.close();
 		},
+		
+		onTypeMismatch: function(event) {
+			MessageBox.error(this.i18n.getText("importOnlyCsv"));
+		},
 
 		/* =========================================================== */
 		/* internal methods											*/
