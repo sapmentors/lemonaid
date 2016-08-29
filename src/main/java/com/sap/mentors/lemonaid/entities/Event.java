7a2ff179532d84880370b0eb28e04b113385498c
Jan Penninkhof <jan@penninkhof.com>
2016-08-29 12:56:35 +0200

    Add events entity

---
+++ src/main/java/com/sap/mentors/lemonaid/entities/Event.java
@@ -0,0 +1,87 @@
+package com.sap.mentors.lemonaid.entities;
+
+import static javax.persistence.TemporalType.TIMESTAMP;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+
+@Entity
+@Table(name="events")
+public class Event {	
+
+	@Id
+	private String id;
+	private String source;
+    private String sourceId;
+    private String location;
+	@Temporal(TIMESTAMP) private Date startDate;
+	@Temporal(TIMESTAMP) private Date endDate;
+	private String url;
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getSource() {
+		return source;
+	}
+	
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	public String getSourceId() {
+		return sourceId;
+	}
+	
+	public void setSourceId(String sourceId) {
+		this.sourceId = sourceId;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	
+	public Date getEndDate() {
+		return endDate;
+	}
+	
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", source=" + source + ", sourceId=" + sourceId + ", location=" + location
+				+ ", startDate=" + startDate + ", endDate=" + endDate + ", url=" + url + "]";
+	}
+
+}
