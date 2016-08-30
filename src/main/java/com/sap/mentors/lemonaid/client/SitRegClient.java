b1ed61c6e7244ae527ba1fedaf9f0b65192a5db8
Jan Penninkhof <jan@penninkhof.com>
2016-08-30 18:15:41 +0200

    Refactoring

--- src/main/java/com/sap/mentors/lemonaid/client/SitRegClient.java
+++ src/main/java/com/sap/mentors/lemonaid/client/SitRegClient.java
@@ -3,17 +3,48 @@ package com.sap.mentors.lemonaid.client;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
 
+import org.apache.olingo.odata2.api.ep.entry.ODataEntry;
 import org.apache.olingo.odata2.api.ep.feed.ODataFeed;
 import org.apache.olingo.odata2.api.exception.ODataException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.sap.mentors.lemonaid.entities.Event;
 
 @Component
 public class SitRegClient extends GenericODataClient {
 
 	public static final String SERVICE_URL = "https://sapmentorsa5a504e08.hana.ondemand.com/com/sap/sapmentors/sitreg/odatapublic/service.xsodata";
 
-	public ODataFeed readFeed(String entitySetName)
+	private static final Logger log = LoggerFactory.getLogger(SitRegClient.class);
+
+	public ArrayList<Event> getEvents() {
+		try {
+			ODataFeed feed = readFeed("Events");
+			ArrayList<Event> events = new ArrayList<Event>();
+			for (ODataEntry entry : feed.getEntries()) {
+				Event event = new Event();
+				event.setSourceId(entry.getProperties().get("ID").toString());
+				event.setSource("SITREG");
+				event.setId(event.getSource() + "-" + event.getSourceId());
+				event.setLocation((String) entry.getProperties().get("Location"));
+				event.setStartDate(((GregorianCalendar) entry.getProperties().get("StartTime")).getTime());
+				event.setEndDate(((GregorianCalendar) entry.getProperties().get("EndTime")).getTime());
+				event.setUrl((String) entry.getProperties().get("HomepageURL"));
+				events.add(event);
+			}
+			return events;
+		} catch (IOException | ODataException e) {
+			log.warn("Unable to retrieve Inside Track events list: " + e.getMessage());
+			return null;
+		}
+	}
+
+	private ODataFeed readFeed(String entitySetName)
 			throws IOException, ODataException {
 		return readFeed(SERVICE_URL, APPLICATION_JSON, entitySetName);
 	}
--- src/main/java/com/sap/mentors/lemonaid/jobs/InsideTrackJob.java
+++ src/main/java/com/sap/mentors/lemonaid/jobs/InsideTrackJob.java
@@ -1,12 +1,7 @@
 package com.sap.mentors.lemonaid.jobs;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 
-import org.apache.olingo.odata2.api.ep.entry.ODataEntry;
-import org.apache.olingo.odata2.api.ep.feed.ODataFeed;
-import org.apache.olingo.odata2.api.exception.ODataException;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -28,7 +23,6 @@ public class InsideTrackJob implements Job {
     
 	private static final Logger log = LoggerFactory.getLogger(InsideTrackJob.class);
 	
-	
 	@Autowired EventRepository eventRepository;
 	@Autowired SitRegClient sitregClient;
 
@@ -36,35 +30,14 @@ public class InsideTrackJob implements Job {
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		log.info("Pulling inside track events - Start");
 		long startTime = System.currentTimeMillis();
-		ArrayList<Event> events = getEvents();
+		ArrayList<Event> events = sitregClient.getEvents();
 		if (events != null) {
+			eventRepository.deleteAll();
 			for (Event event : events) {
 				eventRepository.save(event);
 			}
 		}
 		log.info("Pulling inside track events  - End. Duration: " + Long.toString(System.currentTimeMillis() - startTime) + "ms");
-	}
-
-	private ArrayList<Event> getEvents() {
-		try {
-			ODataFeed feed = sitregClient.readFeed("Events");
-			ArrayList<Event> events = new ArrayList<Event>();
-			for (ODataEntry entry : feed.getEntries()) {
-				Event event = new Event();
-				event.setSourceId(entry.getProperties().get("ID").toString());
-				event.setSource("SITREG");
-				event.setId(event.getSource() + "-" + event.getSourceId());
-				event.setLocation((String) entry.getProperties().get("Location"));
-				event.setStartDate(((GregorianCalendar) entry.getProperties().get("StartTime")).getTime());
-				event.setEndDate(((GregorianCalendar) entry.getProperties().get("EndTime")).getTime());
-				event.setUrl((String) entry.getProperties().get("HomepageURL"));
-				events.add(event);
-			}
-			return events;
-		} catch (IOException | ODataException e) {
-			log.warn("Unable to retrieve Inside Track events list: " + e.getMessage());
-			return null;
-		}
 	}
 
 }
