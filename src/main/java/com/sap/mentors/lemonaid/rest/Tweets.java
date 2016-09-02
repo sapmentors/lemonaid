b293f8c40bada8bf0a1b34c6e11105615b194b15
Jan Penninkhof <jan@penninkhof.com>
2016-09-02 09:53:06 +0200

    Extended twitter service

--- src/main/java/com/sap/mentors/lemonaid/rest/Tweets.java
+++ src/main/java/com/sap/mentors/lemonaid/rest/Tweets.java
@@ -1,54 +1,43 @@
 package com.sap.mentors.lemonaid.rest;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.social.twitter.api.SearchMetadata;
 import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.social.twitter.api.Tweet;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class Tweets {
+
+	private List<Tweet> statuses;
+	private SearchMetadata search_metadata;
 
 	public Tweets(SearchResults results) {
 		for (int i = 0; i < results.getTweets().size(); i++) {
-			statuses.add(new Status(results.getTweets().get(i)));
+			if (statuses == null) this.statuses = new ArrayList<Tweet>(); 
+			this.statuses.add(results.getTweets().get(i));
 		}
-		search_metadata = new SearchMetadata(results.getSearchMetadata());
+		search_metadata = results.getSearchMetadata();
 	}
 
-	List<Status> statuses;
-	SearchMetadata search_metadata;
-	
-	public class Status {
-		
-		String coordinates;
-		boolean favorited;
-		boolean truncated;
-		String created_at;
-		String id_str;
-
-		public Status(Tweet tweet) {
-			this.id_str = tweet.getIdStr();
-		}
-
+	public List<Tweet> getStatuses() {
+		return statuses;
 	}
-	
-	public class SearchMetadata {
-		
-		long max_id;
-		long since_id;
-		String refresh_url;
-		int count;
-		float completed_in;
-		String since_id_str;
-		String query;
-		String max_id_str;
 
-		public SearchMetadata(org.springframework.social.twitter.api.SearchMetadata searchMetadata) {
-			this.max_id = searchMetadata.getMaxId();
-			this.max_id_str = Long.toString(max_id);
-			this.since_id = searchMetadata.getSinceId();
-			this.since_id_str = Long.toString(this.since_id);
-		}
-		
+	public void setStatuses(List<Tweet> statuses) {
+		this.statuses = statuses;
 	}
-	
+
+	public SearchMetadata getSearch_metadata() {
+		return search_metadata;
+	}
+
+	public void setSearch_metadata(SearchMetadata search_metadata) {
+		this.search_metadata = search_metadata;
+	}
+
 }
--- src/main/java/com/sap/mentors/lemonaid/rest/TweetsController.java
+++ src/main/java/com/sap/mentors/lemonaid/rest/TweetsController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.social.twitter.api.SearchResults;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
