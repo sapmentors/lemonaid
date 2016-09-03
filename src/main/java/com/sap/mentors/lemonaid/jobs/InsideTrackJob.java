d3df9f8ec68e242e9cd51cd2b99534c9ee4dbcea
Jan Penninkhof <jan@penninkhof.com>
2016-09-03 23:56:49 +0200

    Only overwrite incoming event data, merge the rest

--- src/main/java/com/sap/mentors/lemonaid/jobs/InsideTrackJob.java
+++ src/main/java/com/sap/mentors/lemonaid/jobs/InsideTrackJob.java
@@ -30,10 +30,23 @@ public class InsideTrackJob implements Job {
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 		log.info("Pulling inside track events - Start");
 		long startTime = System.currentTimeMillis();
-		ArrayList<Event> events = sitregClient.getEvents();
-		if (events != null) {
-			for (Event event : events) {
-				eventRepository.save(event);
+		ArrayList<Event> sitRegEvents = sitregClient.getEvents();
+		if (sitRegEvents != null) {
+			for (Event sitRegEvent : sitRegEvents) {
+				Event event = eventRepository.findOne(sitRegEvent.getId());
+				if (event == null) {
+					eventRepository.save(sitRegEvent);
+				} else {
+					event.setEndDate(sitRegEvent.getEndDate());
+					event.setStartDate(sitRegEvent.getStartDate());
+					event.setId(sitRegEvent.getId());
+					event.setLocation(sitRegEvent.getLocation());
+					event.setName(sitRegEvent.getName());
+					event.setSource(sitRegEvent.getSource());
+					event.setSourceId(sitRegEvent.getSourceId());
+					event.setUrl(sitRegEvent.getUrl());
+					eventRepository.save(event);
+				}
 			}
 		}
 		log.info("Pulling inside track events  - End. Duration: " + Long.toString(System.currentTimeMillis() - startTime) + "ms");
