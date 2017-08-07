86b3b357eef6a5066da7d2fbc3c5919a465dc7da
Lars Langhammer <l.langhammer@sap.com>
2017-08-07 14:49:34 +0200

    Added Headers for getCurrentUser Answer(for IE)

--- src/main/java/com/sap/mentors/lemonaid/rest/UserController.java
+++ src/main/java/com/sap/mentors/lemonaid/rest/UserController.java
@@ -3,6 +3,7 @@ package com.sap.mentors.lemonaid.rest;
 import java.security.Principal;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,16 +23,19 @@ public class UserController {
 	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping(method = RequestMethod.GET)
-    User getUser(HttpServletRequest request) {
-    	Principal userPrincipal = request.getUserPrincipal();
+    User getUser(HttpServletRequest request, HttpServletResponse response) {
+        Principal userPrincipal = request.getUserPrincipal();
+        response.setHeader("Cache-Control","no-cache,no-store,must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setDateHeader("Expires", 0);
 		if (userPrincipal != null) {
             try {
 			    // UserProvider provides access to the user storage
 			    UserProvider users = UserManagementAccessor.getUserProvider();
-	
+
 			    // Read the currently logged in user from the user storage
 			    com.sap.security.um.user.User user = users.getUser(userPrincipal.getName());
-	
+
 			    // Print the user name and email
 			    return new User(
 			    		userPrincipal.getName(),
@@ -42,7 +46,7 @@ public class UserController {
 			    	);
 			} catch (Exception e) {
 				log.error("Error: " + e.getMessage());
-			}			
+			}
 	        return new User(userPrincipal.getName());
 		}
         return new User();
